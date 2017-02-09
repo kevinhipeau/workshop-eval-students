3a035f05422eb1266d0f0b4e265cc339737b41ff
kevinhipeau <kevin.hipeau@gmail.COM>
2017-02-09 11:12:07 +0100

    Exercise 1

--- src/store.js
+++ src/store.js
@@ -38,7 +38,8 @@ function calculateTotalprice(products) {
   for (var i = products.length - 1; i >= 0; i--) {
     sum += products[i].price;
   }
-  return sum;
+
+  return Math.round(sum*100)/100;
 }
 
 function toString(currency, rateCurrency) {
@@ -46,6 +47,7 @@ function toString(currency, rateCurrency) {
   for (var i = products.length - 1; i >= 0; i--) {
     result += '\n - ' + products[i].toString(currency, rateCurrency);
   }
+  console.log("")
   return result;
 }
 
